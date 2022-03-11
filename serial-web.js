df486fd0f05222cd9ae75c2e04ff42bc67b6c391
Vladyslav Novytskyi <novitskyi57@gmail.com>
2022-03-11 15:29:51 +0200

    Insert JQuery code; Remove getReader from cycle

--- serial-web.js
+++ serial-web.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
   if ("serial" in navigator) {
     console.log("[ OK ] The Web Serial API is supported!");
   } else {
@@ -7,50 +7,46 @@ document.addEventListener('DOMContentLoaded', function() {
     return;
   }
 
-  document.getElementById("requestPortsButton").addEventListener("click", async function() {
-    navigator.serial.requestPort()
-      .then(async (port) => {
-        await port.open({baudRate: 115200});
-        console.log("[ OK ] Port opened");
-        console.log("-----------------------");
-        console.log("");
+  $("#requestPortsButton").click(async function() {
+    const port = await navigator.serial.requestPort()
 
-        var outputStr = ""
-        const decoder = new TextDecoder()
+    await port.open({baudRate: 115200});
+    console.log("[ OK ] Port opened");
+    console.log("-----------------------");
+    console.log("");
 
-        while (port.readable) {
-          const reader = port.readable.getReader();
+    const decoder = new TextDecoder()
+    const reader = port.readable.getReader();
 
-          while (true) {
-            const { value, done } = await reader.read();
+    var outputStr = "";
+    while (true) {
+      const { value, done } = await reader.read();
               
-            if (done) {
-              reader.releaseLock();
-              break;
-            }
+      if (done) {
+        reader.releaseLock();
+        break;
+      }
 
-            if (value) {            
-              const newLineIndex = value.indexOf(0x0A);
-              if (newLineIndex !== -1) {
-                if ((newLineIndex + 1) == value.byteLength) {
-                  outputStr += decoder.decode(value);
-                  console.log(outputStr);
-                  outputStr = "";
-                } else {
-                  let end = value.slice(0, newLineIndex + 1);
-                  outputStr += decoder.decode(end);
-                  console.log(outputStr);
+      if (value) {            
+        const newLineIndex = value.indexOf(0x0A);
+        if (newLineIndex !== -1) {
+          if ((newLineIndex + 1) == value.byteLength) {
+            outputStr += decoder.decode(value);
+            console.log(outputStr);
+            outputStr = "";
+          } else {
+            let end = value.slice(0, newLineIndex + 1);
+            outputStr += decoder.decode(end);
+            console.log(outputStr);
 
-                  outputStr = "";
-                  let newBegin = value.slice(newLineIndex + 1, value.byteLength);
-                  outputStr =  decoder.decode(newBegin);
-                }
-              } else {
-                outputStr += decoder.decode(value);
-              }
-            }
+            outputStr = "";
+            let newBegin = value.slice(newLineIndex + 1, value.byteLength);
+            outputStr =  decoder.decode(newBegin);
           }
+        } else {
+          outputStr += decoder.decode(value);
         }
-      });
+      }
+    }
   });
 });
